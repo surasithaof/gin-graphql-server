e4d862cb43c98e23188a89cc46615f9b0ceda8fc
Surasith Kaewvikkit <surasith.kae@ascendcorp.com>
2023-04-16 02:07:10 +0700

    feat: add pg store

--- teams/store.go
+++ teams/store.go
@@ -1,4 +1,14 @@
 package teams
 
+import (
+	"context"
+	"surasithit/gin-graphql-server/teams/model"
+)
+
 type Store interface {
+	FindAll(ctx context.Context) ([]*model.Team, error)
+	FindOne(ctx context.Context, id int) (*model.Team, error)
+	Create(ctx context.Context, team *model.Team) (*model.Team, error)
+	Update(ctx context.Context, id int, team *model.Team) (*model.Team, error)
+	Delete(ctx context.Context, id int) error
 }
